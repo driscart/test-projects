5857458535e4803600e418e6dbe423038dee7399
driscart <driscart@hotmail.es>
2015-07-17 20:25:52 +0200

    archetype test

---
+++ my-archetype/src/main/resources/archetype-resources/src/main/java/org/test/TestPackage.java
@@ -0,0 +1,14 @@
+package ${package}.org.test;
+
+public class TestPackage {
+	
+	private String test;
+	
+	public String getTest() {
+		return test;
+	}
+	public void setTest(String test) {
+		this.test = test;
+	}
+
+}
